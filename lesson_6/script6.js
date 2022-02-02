c878b1f5825356d347a9018203e01643a32b363a
Your Name <kirillkolinko1996@gmail.com>
2022-02-02 21:06:33 +0300

    lesson_6

---
+++ lesson_6/script6.js
@@ -0,0 +1,57 @@
+let age = prompt("Введите Ваш возраст");
+
+if (age >= 18) {
+    alert("Вы допущены!");
+}
+
+if (true) {
+    alert("true");
+}
+
+if (false) {
+    alert("false");
+}
+
+if (age >= 18) {
+    alert("Вы допущены!");
+} else {
+    alert("Вы не допущены!");
+}
+
+if (1) {
+    alert("One");
+} else {
+    alert("Zero!");
+}
+
+if (0) {
+    alert("One");
+} else {
+    alert("Zero!");
+}
+
+if (age == 18) {
+    alert("С совершеннолетием!");
+} else if (age > 18) {
+    alert("Вы допущен!");
+} else {
+    alert("Вы не допущены!");
+} // Вариантов выбора больше , чем два 
+
+// условия проверяется по очереди 
+
+if ("0") {
+    alert("One");
+} else {
+    alert("Zero!");
+}
+
+if (" ") {
+    alert("One");
+} else {
+    alert("Zero!");
+}
+// это не пуская строка тут пробел 
+// пуская строка это false "" пример
+
+// результат сравнения это boolean
